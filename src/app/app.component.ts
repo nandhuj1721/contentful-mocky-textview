06402aaf0c840aa366f5931afb1d498a4ea7b2bd
Nandhu J <43676502+nandhuj1721@users.noreply.github.com>
2021-09-19 23:53:47 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { GetDataService } from './services/get-data.service';
+import{Document} from '@contentful/rich-text-types';
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'contentful-mocky-textview';
+export class AppComponent implements OnInit {
+  public title: any;
+  public imageurl: any;
+  public content!: Document[];
+  categorySection: any;
+  public get!: any;
+  constructor(private data: GetDataService){}
+  ngOnInit(){
+  this.data.fetchTitle().subscribe(res=>{
+    let titleField= res['data'].map((res: { data: { articleTitle: { iv: any; }; }; }) => res.data.articleTitle.iv);
+    let contentField= res['data'].map((res: { data: { articleText: { iv: any; }; }; }) => res.data.articleText.iv);
+    let category= res['data'].map((res: { data: { category: { iv: any; }; }; }) => res.data.category.iv);
+    let imagedata = res['data'].map((res: { data: { showPageImageLink: { iv: any; }; }; }) => res.data.showPageImageLink.iv);
+   // let imagedata2 = res['data'].map((res: { data: { holdingPageImageLink: { iv: any; }; }; }) => res.data.holdingPageImageLink.iv);
+    this.title = titleField  ;
+    this.categorySection= category;
+    this.content = contentField;
+    this.imageurl = imagedata;
+  });
+  }
+
+  fetchHtmlRichText(richText:Document){
+    return documentToHtmlString(richText);
+  }
+  enter(index: number) {
+    this.get = index;
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { GetDataService } from './services/get-data.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [GetDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
